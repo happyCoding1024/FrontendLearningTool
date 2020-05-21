3378b8b37add3cf84ee35166c096129ad6110640
happyCoding1024 <zhangguicheng_1995@163.com>
2020-05-21 22:18:28 +0800

    update 每日知识点分享

--- 面试/手撕代码系列/获取url参数.js
+++ 面试/手撕代码系列/获取url参数.js
@@ -1,19 +1,19 @@
 // 方法一：自己写函数
-// function queryUrlSearch () {
-//   let searArr = {}
-//   let key,value
-//   const search = location.search.substr(1)
-//   const arr = search.split('&')
-//   arr.forEach(item => {
-//     key = item.split('=')[0]
-//     value = item.split('=')[1]
-//     searArr[key] = value
-//   })
-//   return searArr
-// }
+function queryUrlSearch () {
+  let searArr = {}
+  let key,value
+  const search = location.search.substr(1)
+  const arr = search.split('&')
+  arr.forEach(item => {
+    key = item.split('=')[0]
+    value = item.split('=')[1]
+    searArr[key] = value
+  })
+  return searArr
+}
 
-// const res = queryUrlSearch()
-// console.log(res.a)
+const res = queryUrlSearch()
+console.log(res.a)
 
 // 方法2：URLSearchParams
 
