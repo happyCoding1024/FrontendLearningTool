a45eb1c83f0574289e5519d82d56f792957e0d9d
happyCoding1024 <zhangguicheng_1995@163.com>
2020-05-24 16:26:50 +0800

    update code writing

--- 面试/手撕代码系列/获取url参数.js
+++ 面试/手撕代码系列/获取url参数.js
@@ -1,7 +1,8 @@
 // 方法一：自己写函数
 function queryUrlSearch () {
   let searArr = {}
-  let key,value
+  let key, value
+  // eslint-disable-next-line no-undef
   const search = location.search.substr(1)
   const arr = search.split('&')
   arr.forEach(item => {
@@ -17,5 +18,5 @@ console.log(res.a)
 
 // 方法2：URLSearchParams
 
-const res = new URLSearchParams(location.search)
-console.log(res.get('a'))
+// const res = new URLSearchParams(location.search)
+// console.log(res.get('a'))
