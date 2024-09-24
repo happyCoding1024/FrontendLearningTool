c26834a5201a1a1b43a2ebbd544865bf59bfef6e
happyCoding1024 <zhangguicheng_1995@163.com>
2024-09-24 10:49:23 +0800

    feat: 新增每日一题

---
+++ 数据结构与算法/每日一题/696.计数二进制子串.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=696 lang=javascript
+ *
+ * [696] 计数二进制子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countBinarySubstrings = function(s) {
+
+};
+// @lc code=end
+
